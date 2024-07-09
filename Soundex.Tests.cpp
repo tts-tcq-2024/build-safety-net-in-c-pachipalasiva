8ff2ae49ceb983ff05de9763e8dcf0278e007378
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-07-09 17:16:21 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,14 +4,28 @@
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
-  generateSoundex("@$%j", soundex);
+ generateSoundex("@$%j", soundex);
  ASSERT_EQ(soundex,"@200");
+}
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_1) {
+ //AAA
+ char soundex[5];
  generateSoundex("Adams", soundex);
  ASSERT_EQ(soundex,"A352");
-  generateSoundex("Aeiou", soundex);
- ASSERT_EQ(soundex,"A000");
+}
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
+ //AAA
+  char soundex[5];
  generateSoundex("@@gty", soundex);
  ASSERT_EQ(soundex,"@230");
- 
- 
 }
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_vowels) {
+ //AAA
+ char soundex[5];
+ generateSoundex("aeiou", soundex);
+ ASSERT_EQ(soundex,"A000");
+}
+
